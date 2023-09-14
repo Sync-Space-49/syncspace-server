c8f00cff60ba742c1d28bcc2587cac3c4ad1814c
Dylan Jacob Halstead <Dylanhalstead11@gmail.com>
2023-09-14 13:33:34 -0400

    Fixed "Linting"

--- SQL-Setup.sql
+++ SQL-Setup.sql
@@ -1,14 +1,11 @@
-```
-SyncSpace SQL Setup
+-- SyncSpace SQL Setup
 
-Created for https://github.com/Sync-Space-49/syncspace-server/
+-- Created for https://github.com/Sync-Space-49/syncspace-server/
 
-Authors: 
-    Nathan Laney
-    Dylan Halstead
+-- Authors: 
+--     Nathan Laney
+--     Dylan Halstead
 
-https://user-images.githubusercontent.com/70990184/267409122-ddc40017-4d0a-4e29-a707-4858b364b630.png
-``` 
 SET TIMEZONE = "America/New_York"; -- sets timezone for timestamptz datatypes
 
 CREATE TABLE IF NOT EXISTS Users (
@@ -22,7 +19,7 @@ CREATE TABLE IF NOT EXISTS Users (
 CREATE TABLE IF NOT EXISTS Organizations (
     id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
-    description     TEXT 
+    description     TEXT
 );
 
 CREATE TABLE IF NOT EXISTS Organization_Members (
@@ -33,17 +30,17 @@ CREATE TABLE IF NOT EXISTS Organization_Members (
 
 CREATE TABLE IF NOT EXISTS Boards (
     id              SERIAL PRIMARY KEY,
-    title           VARCHAR(255) NOT NULL, -- Changed to Title from Name to match Lists and Cards 
+    title           VARCHAR(255) NOT NULL, -- Changed to Title from Name to match Lists and Cards
     created_at      TIMESTAMPTZ NOT NULL DEFAULT NOW(), -- 'default' calling a function may not work? if not j remove the function call
     modified_at     TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    is_private      BOOLEAN DEFAULT 0;                  -- defaults to public
+    is_private      BOOLEAN DEFAULT FALSE,                  -- defaults to public
     organization_id SERIAL, FOREIGN KEY (organization_id) REFERENCES Organizations(id)
 );
 
 CREATE TABLE IF NOT EXISTS Lists (
     id              SERIAL PRIMARY KEY,
     title           VARCHAR(255) NOT NULL,
-    position        SMALLINT(255), -- changed from int to smallint (will not have more than 32767 lists/cards/etc)
+    position        SMALLINT, -- changed from int to smallint (will not have more than 32767 lists/cards/etc)
     board_id        SERIAL, FOREIGN KEY (board_id) REFERENCES Boards(id)
 );
 
@@ -51,7 +48,7 @@ CREATE TABLE IF NOT EXISTS Cards (
     id              SERIAL PRIMARY KEY,
     title           VARCHAR(255) NOT NULL,
     description     TEXT,
-    position        SMALLINT(255), -- changed from int to smallint (will not have more than 32767 lists/cards/etc)
+    position        SMALLINT, -- changed from int to smallint (will not have more than 32767 lists/cards/etc)
     list_id         SERIAL, FOREIGN KEY (list_id) REFERENCES Lists(id)
 );
 
@@ -62,7 +59,7 @@ CREATE TABLE IF NOT EXISTS Assigned_Cards (
 );
 
 CREATE TABLE IF NOT EXISTS Tags (
-    id              SERIAL PRIMARY KEY, 
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL
 );
 
@@ -72,63 +69,62 @@ CREATE TABLE IF NOT EXISTS Card_Tags (
     card_id         SERIAL, FOREIGN KEY (card_id) REFERENCES Cards(id)
 );
 
-CREATE TABLE IF NOT EXISTS Board_Members ( 
-    id              SERIAL PRIMARY KEY, 
-    member_id       SERIAL, FOREIGN KEY (member_id) REFERENCES Organization_Members(id), 
+CREATE TABLE IF NOT EXISTS Board_Members (
+    id              SERIAL PRIMARY KEY,
+    member_id       SERIAL, FOREIGN KEY (member_id) REFERENCES Organization_Members(id),
     board_id        SERIAL, FOREIGN KEY (board_id) REFERENCES Boards(id)
 );
 
 CREATE TABLE IF NOT EXISTS Board_Roles (
-    id              SERIAL PRIMARY KEY, 
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
-    is_default      BOOLEAN DEFAULT 0
+    is_default      BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS Board_Member_Roles ( 
-    id              SERIAL PRIMARY KEY, 
-    board_member_id SERIAL, FOREIGN KEY (board_member_id) REFERENCES Board_Members(member_id), 
+CREATE TABLE IF NOT EXISTS Board_Member_Roles (
+    id              SERIAL PRIMARY KEY,
+    board_member_id SERIAL, FOREIGN KEY (board_member_id) REFERENCES Board_Members(id),
     board_role_id   SERIAL, FOREIGN KEY (board_role_id) REFERENCES Board_Roles(id)
 );
 
-CREATE TABLE IF NOT EXISTS Board_Privileges ( 
-    id              SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Board_Privileges (
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     description     TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Board_Roles_Privileges ( 
-    id              SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Board_Roles_Privileges (
+    id              SERIAL PRIMARY KEY,
     board_privilege_id
-                    SERIAL, FOREIGN KEY (board_privilege_id) REFERENCES Board_Privileges(id), 
+                    SERIAL, FOREIGN KEY (board_privilege_id) REFERENCES Board_Privileges(id),
     board_role_id   SERIAL, FOREIGN KEY (board_role_id) REFERENCES Board_Roles(id)
 );
 
 CREATE TABLE IF NOT EXISTS Organization_Roles (
-    id              SERIAL PRIMARY KEY, 
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
-    is_default      BOOLEAN DEFAULT 0
+    is_default      BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS Organization_Privileges ( 
-    id              SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Organization_Privileges (
+    id              SERIAL PRIMARY KEY,
     name            VARCHAR(255) NOT NULL,
     description     TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Organization_Member_Roles ( 
-    id              SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Organization_Member_Roles (
+    id              SERIAL PRIMARY KEY,
     organization_role_id
-                    SERIAL, FOREIGN KEY (organization_role_id) REFERENCES Organization_Roles(id), 
+                    SERIAL, FOREIGN KEY (organization_role_id) REFERENCES Organization_Roles(id),
     organization_member_id
                     SERIAL, FOREIGN KEY (organization_member_id) REFERENCES Organization_Members(id)
 );
 
 
-CREATE TABLE IF NOT EXISTS Organization_Role_Priviliges ( 
-    id              SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Organization_Role_Priviliges (
+    id              SERIAL PRIMARY KEY,
     organization_role_id
-                    SERIAL, FOREIGN KEY (organization_role_id) REFERENCES Organization_Roles(id), 
+                    SERIAL, FOREIGN KEY (organization_role_id) REFERENCES Organization_Roles(id),
     organization_privilege_id
                     SERIAL, FOREIGN KEY (organization_privilege_id) REFERENCES Organization_Privileges(id)
 );
-
