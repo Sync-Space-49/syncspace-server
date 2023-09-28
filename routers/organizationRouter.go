19f1912c482d22cd234402c73539d82087cb0b79
DylanHalstead <dylanhalstead11@gmail.com>
2023-09-28 16:37:01 -0400

    commented out proposed code

--- routers/organizationRouter.go
+++ routers/organizationRouter.go
@@ -3,7 +3,6 @@ package routers
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -24,7 +23,7 @@ func registerOrganizationRoutes(cfg *config.Config, db *db.DB) *mux.Router {
 	}
 
 	// Middleware to be added for authorization
-	handler.router.HandleFunc(fmt.Sprintf("%s", organizationsPrefix), handler.CreateOrganization).Methods("POST")
+	handler.router.HandleFunc(organizationsPrefix, handler.CreateOrganization).Methods("POST")
 	handler.router.HandleFunc(fmt.Sprintf("%s/{organizationId}", organizationsPrefix), handler.GetOrganization).Methods("POST")
 	handler.router.HandleFunc(fmt.Sprintf("%s/{organizationId}", organizationsPrefix), handler.UpdateOrganization).Methods("PUT")
 	handler.router.HandleFunc(fmt.Sprintf("%s/{organizationId}", organizationsPrefix), handler.DeleteOrganization).Methods("DELETE")
@@ -48,26 +47,29 @@ func registerOrganizationRoutes(cfg *config.Config, db *db.DB) *mux.Router {
 }
 
 func (handler *organizationHandler) CreateOrganization(writer http.ResponseWriter, request *http.Request) {
+	// TODO: Get organization from request body
+	// TODO: Create organization in database
+	// TODO: Return organization with status code 201
 }
 
 func (handler *organizationHandler) GetOrganization(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: Get organization by ID
 	// TODO: Return organization with status code 201
 }
 
 func (handler *organizationHandler) UpdateOrganization(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: Find what fields are being updated
 	// TODO: Get organization by ID
 	// TODO: update organization in database
@@ -75,130 +77,130 @@ func (handler *organizationHandler) UpdateOrganization(writer http.ResponseWrite
 }
 
 func (handler *organizationHandler) DeleteOrganization(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user is owner of organization
 	// TODO: Delete organization from database
 	// TODO: send back 204
 }
 
 func (handler *organizationHandler) GetUserOrganizations(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	userId, err := strconv.Atoi(params["userId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// userId, err := strconv.Atoi(params["userId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user is signed in user (posisbly with middleware)
 	// TODO: Get organizations by user ID
 	// TODO: Return organizations with status code 200
 }
 
 func (handler *organizationHandler) GetOrganizationMembers(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request is apart of organization (posisbly with middleware)
 	// TODO: Get members by organization ID
 	// TODO: Return members with status code 200
 }
 
 func (handler *organizationHandler) AddMemberToOrganization(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can add members to organization (posisbly with middleware)
 	// TODO: Add member to organization
 	// TODO: Return member with status code 201
 }
 
 func (handler *organizationHandler) GetOrganizationMember(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	memberId, err := strconv.Atoi(params["memberId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// memberId, err := strconv.Atoi(params["memberId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request is apart of organization (posisbly with middleware)
 	// TODO: Get member by organization ID and member ID
 	// TODO: Return member with status code 200
 }
 
 func (handler *organizationHandler) RemoveMemberFromOrganization(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	memberId, err := strconv.Atoi(params["memberId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// memberId, err := strconv.Atoi(params["memberId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can remove members from organization (posisbly with middleware)
 	// TODO: Remove member from organization
 	// TODO: send back 204
 }
 
 func (handler *organizationHandler) GetOrganizationRoles(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request is apart of organization (posisbly with middleware)
 	// TODO: Get roles by organization ID
 	// TODO: Return roles with status code 200
 }
 
 func (handler *organizationHandler) AddOrganizationRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can add roles to organization (posisbly with middleware)
 	// TODO: Add role to organization
 	// TODO: Return role with status code 201
 }
 
 func (handler *organizationHandler) GetOrganizationRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request is apart of organization (posisbly with middleware)
 	// TODO: Get role by organization ID and role ID
 	// TODO: Return role with status code 200
 }
 
 func (handler *organizationHandler) UpdateOrganizationRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can update roles in organization (posisbly with middleware)
 	// TODO: Find what fields are being updated
 	// TODO: Get role by organization ID and role ID
@@ -207,46 +209,46 @@ func (handler *organizationHandler) UpdateOrganizationRole(writer http.ResponseW
 }
 
 func (handler *organizationHandler) DeleteOrganizationRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can delete roles in organization (posisbly with middleware)
 	// TODO: Delete role from database
 	// TODO: send back 204
 }
 
 func (handler *organizationHandler) GetOrganizationRolePrivileges(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	roleId, err := strconv.Atoi(params["roleId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// roleId, err := strconv.Atoi(params["roleId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request is apart of organization (posisbly with middleware)
 	// TODO: Get privileges by organization ID and role ID
 	// TODO: Return role privileges with status code 200
 }
 
 func (handler *organizationHandler) AddOrganizationRolePrivilege(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	roleId, err := strconv.Atoi(params["roleId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// roleId, err := strconv.Atoi(params["roleId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can add privileges to role in organization (posisbly with middleware)
 	// TODO: Add privilege to role
 	// TODO: Return privilege with status code 201
@@ -254,61 +256,61 @@ func (handler *organizationHandler) AddOrganizationRolePrivilege(writer http.Res
 }
 
 func (handler *organizationHandler) RemoveOrganizationRolePrivilege(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	roleId, err := strconv.Atoi(params["roleId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// roleId, err := strconv.Atoi(params["roleId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can remove privileges from role in organization (posisbly with middleware)
 	// TODO: Remove privilege from role
 	// TODO: send back 204
 }
 
 func (handler *organizationHandler) AddMemberToRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	memberId, err := strconv.Atoi(params["memberId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	roleId, err := strconv.Atoi(params["roleId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// memberId, err := strconv.Atoi(params["memberId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// roleId, err := strconv.Atoi(params["roleId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can add members to role in organization (posisbly with middleware)
 	// TODO: Add member to role
 	// TODO: Return with status code 204
 }
 
 func (handler *organizationHandler) RemoveMemberFromRole(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	organizationId, err := strconv.Atoi(params["organizationId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	memberId, err := strconv.Atoi(params["memberId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	roleId, err := strconv.Atoi(params["roleId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// organizationId, err := strconv.Atoi(params["organizationId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Organization ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// memberId, err := strconv.Atoi(params["memberId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Member ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// roleId, err := strconv.Atoi(params["roleId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid Role ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify user sending request can remove members from role in organization (posisbly with middleware)
 	// TODO: Remove member from role
 	// TODO: Return with status code 204
--- routers/userRouter.go
+++ routers/userRouter.go
@@ -1,17 +1,17 @@
 package routers
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"io"
+	// "io"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strconv"
-	"time"
+	// "os"
+	// "path/filepath"
+	// "strconv"
+	// "time"
 
 	"github.com/gorilla/mux"
-	"golang.org/x/crypto/bcrypt"
+	// "golang.org/x/crypto/bcrypt"
 
 	"github.com/Sync-Space-49/syncspace-server/config"
 	"github.com/Sync-Space-49/syncspace-server/controllers/user"
@@ -39,84 +39,84 @@ func registerUserRoutes(cfg *config.Config, db *db.DB) *mux.Router {
 }
 
 func (handler *userHandler) GetUser(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	userId, err := strconv.Atoi(params["userId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// userId, err := strconv.Atoi(params["userId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 
-	user, err := handler.controller.GetUserById(userId)
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Failed to get user: %s", err.Error()), http.StatusInternalServerError)
-		return
-	}
+	// user, err := handler.controller.GetUserById(userId)
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Failed to get user: %s", err.Error()), http.StatusInternalServerError)
+	// 	return
+	// }
 
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(http.StatusOK)
-	json.NewEncoder(writer).Encode(user)
+	// writer.Header().Set("Content-Type", "application/json")
+	// writer.WriteHeader(http.StatusOK)
+	// json.NewEncoder(writer).Encode(user)
 }
 
 func (handler *userHandler) SignUpUser(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	username := params["username"]
-	email := params["email"]
-	password := params["password"]
-	if email == "" || password == "" || username == "" {
-		http.Error(writer, "Invalid email or password", http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// username := params["username"]
+	// email := params["email"]
+	// password := params["password"]
+	// if email == "" || password == "" || username == "" {
+	// 	http.Error(writer, "Invalid email or password", http.StatusBadRequest)
+	// 	return
+	// }
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		http.Error(writer, "Failed to hash password", http.StatusInternalServerError)
-		return
-	}
+	// hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	// if err != nil {
+	// 	http.Error(writer, "Failed to hash password", http.StatusInternalServerError)
+	// 	return
+	// }
 
-	err = request.ParseMultipartForm(10 << 20) // 10 MB limit on pfp size
-	if err != nil {
-		http.Error(writer, "Unable to parse form (is image too large?)", http.StatusBadRequest)
-		return
-	}
+	// err = request.ParseMultipartForm(10 << 20) // 10 MB limit on pfp size
+	// if err != nil {
+	// 	http.Error(writer, "Unable to parse form (is image too large?)", http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: abstract this into function to be used in other places
-	file, header, err := request.FormFile("profile_picture")
-	if err != nil {
-		http.Error(writer, "Unable to retrieve profile picture", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
+	// file, header, err := request.FormFile("profile_picture")
+	// if err != nil {
+	// 	http.Error(writer, "Unable to retrieve profile picture", http.StatusBadRequest)
+	// 	return
+	// }
+	// defer file.Close()
 
-	fileExtension := filepath.Ext(header.Filename)
-	filename := fmt.Sprintf("%s-%s%s", username, time.Now().Format(time.RFC3339), fileExtension)
+	// fileExtension := filepath.Ext(header.Filename)
+	// filename := fmt.Sprintf("%s-%s%s", username, time.Now().Format(time.RFC3339), fileExtension)
 
-	destinationFile, err := os.Create(filename)
-	if err != nil {
-		http.Error(writer, "Unable to create or open destination file", http.StatusInternalServerError)
-		return
-	}
-	defer destinationFile.Close()
-	_, err = io.Copy(destinationFile, file)
-	if err != nil {
-		http.Error(writer, "Unable to copy file", http.StatusInternalServerError)
-		return
-	}
+	// destinationFile, err := os.Create(filename)
+	// if err != nil {
+	// 	http.Error(writer, "Unable to create or open destination file", http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer destinationFile.Close()
+	// _, err = io.Copy(destinationFile, file)
+	// if err != nil {
+	// 	http.Error(writer, "Unable to copy file", http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// TODO: Add pfp to bucket
 	// TODO: Add user to database
 	// TODO: Add user to their organization
-	writer.Header().Set("Content-Type", "application/json")
-	writer.WriteHeader(http.StatusCreated)
-	json.NewEncoder(writer).Encode(user)
+	// writer.Header().Set("Content-Type", "application/json")
+	// writer.WriteHeader(http.StatusCreated)
+	// json.NewEncoder(writer).Encode(user)
 }
 
 func (handler *userHandler) SignInUser(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	credential := params["credential"]
-	password := params["password"]
-	if credential == "" || password == "" {
-		http.Error(writer, "Invalid email or password", http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// credential := params["credential"]
+	// password := params["password"]
+	// if credential == "" || password == "" {
+	// 	http.Error(writer, "Invalid email or password", http.StatusBadRequest)
+	// 	return
+	// }
 
 	// TODO: GetUserByCredentials (email or username)
 	// Send specific error if user not found/error fr thrown
@@ -139,27 +139,27 @@ func (handler *userHandler) SignInUser(writer http.ResponseWriter, request *http
 func (handler *userHandler) UpdateUser(writer http.ResponseWriter, request *http.Request) {
 	// TODO: verify this is signed in user (possilby with middleware)
 	// Need some logic checking what here the user is updating
-	params := mux.Vars(request)
-	userId, err := strconv.Atoi(params["userId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	email := params["email"]
-	username := params["username"]
-	password := params["password"]
+	// params := mux.Vars(request)
+	// userId, err := strconv.Atoi(params["userId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
+	// email := params["email"]
+	// username := params["username"]
+	// password := params["password"]
 	// TODO: see if user is updating profile picture (possilby handle this in abstracted function)
 	// TODO: update user in database
 	// TODO: send back 204
 }
 
 func (handler *userHandler) DeleteUser(writer http.ResponseWriter, request *http.Request) {
-	params := mux.Vars(request)
-	userId, err := strconv.Atoi(params["userId"])
-	if err != nil {
-		http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
-		return
-	}
+	// params := mux.Vars(request)
+	// userId, err := strconv.Atoi(params["userId"])
+	// if err != nil {
+	// 	http.Error(writer, fmt.Sprintf("Invalid User ID: %s", err.Error()), http.StatusBadRequest)
+	// 	return
+	// }
 	// TODO: verify this is signed in user (possilby with middleware)
 	// TODO: Delete user from database
 	// TODO: send back 204
