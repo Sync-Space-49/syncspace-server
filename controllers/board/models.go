48e8c05849504c063bccefe1737b30710c2326d6
Nathan Laney <nathaniel.laney@gmail.com>
2023-10-02 11:40:06 -0400

    Create model for board and org

--- controllers/board/models.go
+++ controllers/board/models.go
@@ -1,1 +1,10 @@
 package board
+
+type Board struct {
+	Id             int    `db:"id" json:"id"`
+	Title          string `db:"title" json:"title"`
+	CreatedAt      string `db:"created_at" json:"created_at"`
+	ModifiedAt     string `db:"modified_at" json:"modified_at"`
+	IsPrivate      bool   `db:"is_private" json:"is_private"`
+	OrganizationId int    `db:"organization_id" json:"organization_id"`
+}
--- controllers/organization/models.go
+++ controllers/organization/models.go
@@ -1,1 +1,7 @@
 package organization
+
+type Organization struct {
+	Id          int    `db:"id" json:"id"`
+	Name        string `db:"name" json:"name"`
+	Description string `db:"description" json:"description"`
+}
