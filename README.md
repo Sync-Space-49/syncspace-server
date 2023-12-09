58f608d89e26b6b8823c3696a95d59d6348adf1f
Nathan Laney <nathaniel.laney@gmail.com>
2023-12-08 20:01:10 -0500

    Add branding (#106)

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # SyncSpace Server
 ## What Does it Do 🤔
-Syncspace backend is the REST API that handles all communication between the frontend and backend services. It handles all data storage and retrieval for the project. It also handles authentication and authorization for the project ultilizing JSON Web Tokens.
+Syncspace backend is the REST API that handles all communication between the frontend and backend services. It handles all data storage and retrieval for the project. It also handles authentication and authorization for the project ultilizing JSON Web Tokens. 
 
+### Where can I try it out? 
+This code is deployed on Google Cloud, accessible at the following link: https://syncspace-server-i6acbs4ioa-ue.a.run.app.
+
+The hosted version responds to the calls for the hosted [Web](https://github.com/Sync-Space-49/syncspace-web) and [Mobile](https://github.com/Sync-Space-49/syncspace-mobile) versions. This live instance also interacts with a personally hosted PostgreSQL database and a live instance of our [AI layer](https://github.com/Sync-Space-49/syncspace-ai). 
 
 ## 👩‍💻 Local Development
 ### Installation ⚒️
@@ -28,10 +32,3 @@ You can download the project's Go dependencies using the `go get` command. To ru
 ### Compiling 🏗️
 To compile the project to a binary executable, you can run `go build` in the root directory. This is useful for deploying the project but not usually needed for local development.
 
-
-### Tests 🧪
-TODO
-
-
-### Deployment 📦
-TODO
--- main.go
+++ main.go
@@ -1,3 +1,37 @@
+// ____                       ____
+// / ___| _   _ _ __   ___    / ___| _ __   __ _  ___ ___
+// \___ \| | | | '_ \ / __|___\___ \| '_ \ / _` |/ __/ _ \
+//  ___) | |_| | | | | (_|_____|__) | |_) | (_| | (_|  __/
+// |____/ \__, |_| |_|\___|   |____/| .__/ \__,_|\___\___|
+//        |___/                     |_|
+//
+//                      .';codxkOOkxdoc;'.
+//                   ,lxKNMMMMWWWWWWMMMWN0xl'
+//                .lONMMWKkdl:;,''',:cokKWMMNOl,..
+//              .lKWMWKo;.              .,lONMMWXK0Okdl;'.
+//             ,OWMWO:.                     ,xNMMMWNXWMMN0d;.
+//            :XMMKc.                         ;0WMWO;,cd0WMNO,
+//           :XMWO'                            .kWMWd.  .oNMM0,
+//        .:kXMMO'                              .kWMNc   '0MMX:
+//      ,dKWMMMX;                                ;XMMO. .oNMWk.
+//    ,kNMNKXMMk.                                .kMMNc'xNMWk.
+//  .oNMWO;.dMMo                                  dMMWXXWW0:.
+// .dWMXl.  lWMd                                 ,0MMMMNk:.
+// ;XMMx.   :NM0'                           .':dONMMMNx,
+// ;XMM0,   .xMWo.          .oxoc:,..     .;kNMMWWMMWx.
+// .lNMMXx:'.;KMNo.          .OMMMWX0ko,   .x0xloKMMK,
+//   ,dKWMMN0OXMMNOdooooddxkONMMMMMMMWO,    .. ,0WMK;
+//     .;cdk0XXWMMMMMMMMMWWNXXWMMMMW0c.      .cKMW0,
+//          ...,xNMMMXd:;,     dWN0c.      .cOWMXo.
+//               ,dXMWXxc'.   'l:'.    .':dKWWKd'
+//                 .lkXWMNKkdolcc:clodk0NMWKx:.
+//                    .;oxOKNWWWWWWWNX0kdc,.
+//                         ..,,,,,,,'..
+//
+// Authors:
+// Nathan Laney, Dylan Halstead
+// 2023
+
 package main
 
 import (
